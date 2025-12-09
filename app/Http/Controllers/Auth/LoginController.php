9c4e2f73b060b27fd685a51575968f78d6b92591
osamamohti2002 <99078852@mydavinci.nl>
2025-12-09 01:49:43 +0100

    LoginCOntroller aangemaakt en login logica geschreven redirect op basis van rol ook gemaakt

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm(){
+        return view('index');
+    }
+
+    public function login(Request $request){
+
+        // hiermee valideer ik de gegevens die we binnen krijgen
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:6'],
+        ]);
+
+        //check op de gegevens
+
+        if(!Auth::attempt($credentials)){
+            return back()->withErrors([
+                'email' => 'Emailadres of wachtwoord zijn onjuist. ',
+            ])->onlyInput('email');
+        }
+
+        $request->session()->regenerate();
+
+        $role = Auth::user()->role->name;
+
+        switch($role){
+            case 'admin';
+                return redirect()->route(route: 'admin.dashboard');
+
+            case 'planner';
+                return redirect()->route(route: 'planner.dashboard');
+
+            case 'zorgpersoneel';
+                return redirect()->route(route: 'zorg.dashboard');
+        }
+
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('index');
