c10d3184adc1e3c8ffddaf9458aad1a8a8f74dfc
oskahr <oskarsluis@gmail.com>
2025-12-10 10:44:59 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+    
+    Route::get('/create', function () {
+        return view('admin.create');
+    });
+
+    Route::get('/edit', function () {
+        return view('admin.edit');
+    });
+});
