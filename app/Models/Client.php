5a29591bbd3682b44681b03c18b456c15b7639dd
mojtaba332 <99069914@mydavinci.nl>
2026-01-30 10:57:34 +0100

    HasFactory was vergeten

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\ClientZorgMoment;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Client extends Model
 {
